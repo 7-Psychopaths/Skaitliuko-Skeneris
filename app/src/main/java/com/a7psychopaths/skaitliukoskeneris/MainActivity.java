6f784ccdd2fa816214b34f2c570803f9db170efb
Augutis <jercau@gmail.com>
2018-03-21 19:09:15 +0200

    Bottom Navigation

--- app/src/main/java/com/a7psychopaths/skaitliukoskeneris/MainActivity.java
+++ app/src/main/java/com/a7psychopaths/skaitliukoskeneris/MainActivity.java
@@ -1,7 +1,14 @@
 package com.a7psychopaths.skaitliukoskeneris;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView =  (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_add:
+                        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.action_search:
+                        Toast.makeText(MainActivity.this, "Action Search Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
