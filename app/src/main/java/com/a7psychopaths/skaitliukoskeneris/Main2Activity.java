5f4a0fcc97cc2f7511fbbaea24d08db838696370
Augutis <jercau@gmail.com>
2018-04-26 02:44:54 +0300

    Added data posting function to database

--- app/src/main/java/com/a7psychopaths/skaitliukoskeneris/Main2Activity.java
+++ app/src/main/java/com/a7psychopaths/skaitliukoskeneris/Main2Activity.java
@@ -1,10 +1,12 @@
 package com.a7psychopaths.skaitliukoskeneris;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Calendar;
+import java.util.Random;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -13,32 +15,61 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        final BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+
+        final Calendar c = Calendar.getInstance();
+        final int year = c.get(Calendar.YEAR);
+        final int month = c.get(Calendar.MONTH)+1;
+
+        System.out.println();
 
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+//                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//                startActivity(intent);
+                String value = gen()+"";
+                String date = year+"-"+month;
+                String type = "dujos";
+                backgroundWorker.execute(type, value, date);
+                Main2Activity.this.finish();
             }
         });
 
         final Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+//                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//                startActivity(intent);
+
+                String value = gen()+"";
+                String date = year+"-"+month;
+                String type = "elektra";
+                backgroundWorker.execute(type, value, date);
+                Main2Activity.this.finish();
             }
         });
 
         final Button button3 = findViewById(R.id.button3);
         button3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+//                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//                startActivity(intent);
+
+                String value = gen()+"";
+                String date = year+"-"+month;
+                String type = "vanduo";
+                backgroundWorker.execute(type, value, date);
+                Main2Activity.this.finish();
             }
         });
 
 
 
+    }
+
+    public int gen() {
+        Random r = new Random( System.currentTimeMillis() );
+        return ((1 + r.nextInt(2)) * 10000 + r.nextInt(10000));
     }
 }
